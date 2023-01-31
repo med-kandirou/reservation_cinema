eb5006e2ba73d290460b2425d2b96cded9776897
kandirou0 <simo.kandirou15@gmail.com>
2023-01-31 19:40:59 +0100

    update

--- Front-end/src/views/login.vue
+++ Front-end/src/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <Header isUser="false" isGuest="true"/>
+    <Header isUser="false" />
     <div class="grid place-items-center z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
         <div class="w-full h-full max-w-md md:h-auto">
             <!-- Modal content -->
