5e75f1df6b5c6ee8a7d2d1ee69f797ccb68c8c97
kandirou0 <simo.kandirou15@gmail.com>
2023-02-01 11:47:05 +0100

    update

--- Front-end/src/components/movies.vue
+++ Front-end/src/components/movies.vue
@@ -9,6 +9,7 @@
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ name }}</h5>
         </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Salle : {{ salle }}</p>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ date }}</p>
         <button :value="id_f" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Réserver
@@ -28,7 +29,8 @@
         id_f:String,
         name:String,
         date:String,
-        image:String
+        image:String,
+        salle:String
     }
 }
 
