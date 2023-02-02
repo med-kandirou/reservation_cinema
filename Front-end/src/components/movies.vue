19edfa7e57d40b251b61e17813be5c683aa8b395
kandirou0 <simo.kandirou15@gmail.com>
2023-02-02 14:21:13 +0100

    update

--- Front-end/src/components/movies.vue
+++ Front-end/src/components/movies.vue
@@ -3,7 +3,7 @@
 
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+        <img class="rounded-t-lg" :src="image" alt="" />
     </a>
     <div class="p-5">
         <a href="#">
--- Front-end/src/views/movies.vue
+++ Front-end/src/views/movies.vue
@@ -2,7 +2,7 @@
     <h1 class="mt-10 mb-9 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-900">Les films</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         <div v-for="movie in movies">
-            <Movies @getfilm="getfilm" :id_f="movie.id_f" :name="movie.nom_film" :image="movie.image" :date="movie.date_f" :salle="movie.nom_salle" />
+            <Movies @getfilm="getfilm" :id_f="movie.id_f" :name="movie.nom_film" :image="movie.nom_film" :date="movie.date_f" :salle="movie.nom_salle" />
         </div>      
     </div>
 
