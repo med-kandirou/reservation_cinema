bdfd33f1298c8a1f628187cae32a75e86231c396
kandirou0 <simo.kandirou15@gmail.com>
2023-03-01 22:34:45 +0100

    update

--- Front-end/src/views/movies.vue
+++ Front-end/src/views/movies.vue
@@ -2,7 +2,7 @@
 
     <h1 class="mt-10 mb-9 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-900">Les films</h1>
     <div class="flex justify-end mb-10 mr-10">
-        <input type="date" @change="getmovies" v-model="currentDate" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required>
+        <input :min="currentDate" type="date" @change="getmovies" v-model="currentDate" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         <div v-for="movie in movies">
