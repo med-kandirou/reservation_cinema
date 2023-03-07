2e80fa63da1efc902cb0217a361559d908964db7
kandirou0 <simo.kandirou15@gmail.com>
2023-03-07 13:52:50 +0100

    update

--- Front-end/src/components/movies.vue
+++ Front-end/src/components/movies.vue
@@ -3,7 +3,7 @@
 
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img class="rounded-t-lg" :src="image" alt="" />
+        <img class="rounded-t-lg w-full h-65" :src="image" alt="" />
     </a>
     <div class="p-5">
         <a href="#">
